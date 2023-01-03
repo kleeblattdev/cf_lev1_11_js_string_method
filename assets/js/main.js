89a3de010a8e5667869080338a67c7bc2159b342
karinlee <karinlee92@gmail.com>
2023-01-03 13:36:45 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+let x = "Hello";
+
+let y = "World <br>";
+
+let z = x + y;
+
+document.write(z);
+
+let n = x + " " + y;
+
+document.write(n);
+
+document.write(x + "World");
+
+let meinString = "Ich bin Erster";
+
+let zweiterString = "Ich komme auf Platz zwei";
+
+meinString+= " " + zweiterString
+
+console.log(meinString);
